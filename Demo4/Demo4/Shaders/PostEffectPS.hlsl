3a5be644a7e70e62a74fcbef46a5940f3452a755
El cremas <eduexile@gmail.com>
2020-06-16 00:52:24 +0200

    aaa

--- Demo4/Demo4/Shaders/PostEffectPS.hlsl
+++ Demo4/Demo4/Shaders/PostEffectPS.hlsl
@@ -16,21 +16,21 @@ float rgb2luma(float3 rgb);
 float3 main( OutputVS input ) : SV_TARGET
 {
 	FxaaFloat2 pos = float2(0.0,0.0);
-	FxaaFloat4 fxaaConsolePosPos = float4(0,0,0,0);
+	FxaaFloat4 fxaaConsolePosPos = float4(0,0,0,0);			//Consola	
 	FxaaTex tex = { samplerState, sceneTexture };
 	FxaaTex fxaaConsole360TexExpBiasNegOne = { samplerState, sceneTexture };
 	FxaaTex fxaaConsole360TexExpBiasNegTwo = { samplerState, sceneTexture };
 	FxaaFloat2 fxaaQualityRcpFrame = float2(1.0 / (1280.0), 1.0 / (720.0));
-	FxaaFloat4 fxaaConsoleRcpFrameOpt = float4(0, 0, 0, 0);
-	FxaaFloat4 fxaaConsoleRcpFrameOpt2 = float4(0, 0, 0, 0);
-	FxaaFloat4 fxaaConsole360RcpFrameOpt2 = float4(0, 0, 0, 0);
+	FxaaFloat4 fxaaConsoleRcpFrameOpt = float4(0, 0, 0, 0);			//Consola	
+	FxaaFloat4 fxaaConsoleRcpFrameOpt2 = float4(0, 0, 0, 0);		//Consola	
+	FxaaFloat4 fxaaConsole360RcpFrameOpt2 = float4(0, 0, 0, 0);		//Consola	
 	FxaaFloat fxaaQualitySubpix = float(1.0);
 	FxaaFloat fxaaQualityEdgeThreshold = float(0.063);
 	FxaaFloat fxaaQualityEdgeThresholdMin = float(0.0312);
-	FxaaFloat fxaaConsoleEdgeSharpness = float(4.0);
-	FxaaFloat fxaaConsoleEdgeThreshold = float(0.125);
-	FxaaFloat fxaaConsoleEdgeThresholdMin = float(0.05);
-	FxaaFloat4 fxaaConsole360ConstDir = float4(1.0, -1.0, 0.25, -0.25);
+	FxaaFloat fxaaConsoleEdgeSharpness = float(4.0);		//Consola	
+	FxaaFloat fxaaConsoleEdgeThreshold = float(0.125);		//Consola	
+	FxaaFloat fxaaConsoleEdgeThresholdMin = float(0.05);	//Consola	
+	FxaaFloat4 fxaaConsole360ConstDir = float4(1.0, -1.0, 0.25, -0.25);		//Consola	
 
 	float3 colorCenter = sceneTexture.Sample(samplerState, input.texcoord).rgb;
 	float3 fragColor;
